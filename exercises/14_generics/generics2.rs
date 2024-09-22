da755f29a0835df5da9d66be7576daea7a898d8f
@hejj <hejj@tsintergy.com>
2024-09-22 11:09:05 +0800

    feat: Trait

--- exercises/14_generics/generics2.rs
+++ exercises/14_generics/generics2.rs
@@ -1,12 +1,17 @@
 // This powerful wrapper provides the ability to store a positive integer value.
 // TODO: Rewrite it using a generic so that it supports wrapping ANY type.
-struct Wrapper {
-    value: u32,
+struct Wrapper<T> {
+    value: T,
 }
 
 // TODO: Adapt the struct's implementation to be generic over the wrapped value.
-impl Wrapper {
-    fn new(value: u32) -> Self {
+
+// impl 可以实现泛型的某一种类型的 具体实现!
+// impl Wrapper<u32>
+// impl Wrapper<&str> 
+
+impl<T> Wrapper<T> {
+    fn new(value: T) -> Self {
         Wrapper { value }
     }
 }
--- exercises/15_traits/traits5.rs
+++ exercises/15_traits/traits5.rs
@@ -19,7 +19,8 @@ impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // TODO: Fix the compiler error by only changing the signature of this function.
-fn some_func(item: ???) -> bool {
+// impl SomeTrait + OtherTrait 表示同时实现这两个Trait的结构
+fn some_func(item: impl SomeTrait + OtherTrait) -> bool {
     item.some_function() && item.other_function()
 }
 
